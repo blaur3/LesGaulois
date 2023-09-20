acd417b32f820ea84aca99a7d2c75b2e7f3e990d
Celina Bant <laure.bantsoukissa@univ-tlse3.fr>
2023-09-20 14:59:33 +0200

    Creation de methode pour le Druide

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,9 +1,11 @@
 package personnages;
+import java.util.Random;
 
 public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
+	private int forcePotion = 1;
 	
 	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
 		super();
@@ -24,6 +26,26 @@ public class Druide {
 	
 	public String prendreParole() {
 		return "Le druide " + nom + " : ";
+	}
+	
+	public void preparerPotion() {
+		Random random = new Random();
+		int effet = random.nextInt(effetPotionMax+1);
+		while(effet<effetPotionMin) {
+			effet = random.nextInt(effetPotionMax+1);
+		}
+		if(effet >7) {
+			System.out.println("J'ai préparé une super potion de force" + effet);
+		}
+		else {
+			System.out.println("Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force" + effet);
+
+		}
+	}
+	public static void main(String[] args) {
+		Druide panoramix = new Druide("Panoramix",5,10);
+		panoramix.preparerPotion();
+		
 	}
 	
 }
